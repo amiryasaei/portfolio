2e3f24922f5576f96797199ef9b5fe5207157541
amiry4 <amiry@tali.ai>
2024-01-11 08:00:21 -0500

    navbar is completed

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,35 +1,58 @@
 import React, { useState } from 'react';
 import { useIsMobile } from '../../utils/isMobile';
-import mobileMenu from '../../assets/images/mobile-menu.svg';
+import mobileMenuIcon from '../../assets/images/mobileMenuIcon.svg';
 import closeIcon from '../../assets/images/closeIcon.svg';
 import './index.scss';
+import { Link, useLocation } from 'react-router-dom';
+import {clsx} from 'clsx';
 
 export const NavBar = () => {
   const [menuBarClicked, setMenuBarClicked] = useState(false);
   const { isMobile } = useIsMobile();
+  const location = useLocation();
 
   return (
     <>
       {isMobile && !menuBarClicked ? (
         <div className='mobileNavBar'>
           <div className='nameLogo'>AMIR</div>
-          <button onClick={() => setMenuBarClicked(true)}>
-            <img src={mobileMenu} alt='menuLogo' />
+          <button className='menuButton' onClick={() => setMenuBarClicked(true)}>
+            <img src={mobileMenuIcon} alt='menuLogo' />
           </button>
         </div>
       ) : isMobile && menuBarClicked ? (
         <div className='mobileMenu'>
-          <button onClick={() => setMenuBarClicked(false)}>
-            <img src={closeIcon} alt='closeMenu'/>
-          </button>
+          <div className='topBar'>
+            <div className='nameLogo'>AMIR</div>
+            <button className='menuButton' onClick={() => setMenuBarClicked(false)}>
+                <img src={closeIcon} alt='closeMenu'/>
+            </button>
+          </div>
+          <div className='linkEnteries'>                
+            <Link to={'./'} onClick={()=> setMenuBarClicked(false)}>
+              <div className={clsx(location.pathname==='/'?'activeLinkContainer':'linkContainer')}>Home</div>
+            </Link>
+            <Link to={'/about'} onClick={()=> setMenuBarClicked(false)}>
+              <div className={clsx(location.pathname==='/about'?'activeLinkContainer':'linkContainer')}>About</div>
+            </Link>
+            <Link to={'/Contact'} onClick={()=> setMenuBarClicked(false)}>
+              <div className={clsx(location.pathname==='/contact'?'activeLinkContainer':'linkContainer')}>Contact Me</div>
+            </Link>
+          </div>
         </div>
       ) : !isMobile ? (
         <div className='navBar'>
           <div className='nameLogo'>AMIR</div>
           <div className='optionWrapper'>
-            <div>Home</div>
-            <div>About</div>
-            <div>Contact Me</div>
+          <Link to={'./'} onClick={()=> setMenuBarClicked(false)}>
+              <div className={clsx(location.pathname==='/'?'activeLinkContainer':'linkContainer')}>Home</div>
+            </Link>
+            <Link to={'/about'} onClick={()=> setMenuBarClicked(false)}>
+              <div className={clsx(location.pathname==='/about'?'activeLinkContainer':'linkContainer')}>About</div>
+            </Link>
+            <Link to={'/Contact'} onClick={()=> setMenuBarClicked(false)}>
+              <div className={clsx(location.pathname==='/contact'?'activeLinkContainer':'linkContainer')}>Contact Me</div>
+            </Link>
           </div>
         </div>
       ): null}
