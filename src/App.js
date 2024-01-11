0c6ff4bca94504de76aa94b1ae000aa698dc23fa
amiry4 <amiry@tali.ai>
2024-01-11 10:56:33 -0500

    no commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import Layout from './components/Layout'
 import Home from './components/Home'
-import About from './components/About'
+//import Projects from './components/projects'
 import './App.scss';
 import { Route, Routes } from 'react-router-dom';
 import Contact from './components/Contact';
-import bgImage from './assets/images/bgImage.jpg';
 
 
 function App() {
 
   return (
     <>
-    <Routes>
-      <Route path="/" element = {<Layout />}>
-        <Route index element={<Home />}></Route>
-        <Route path='about' element={<About />}></Route>
-        <Route path='contact' element={<Contact />}></Route>
-      </Route>
-    </Routes>
-
-    <img className="video-background" src={bgImage} alt='bg'/>
+      <Routes>
+        <Route path="/" element = {<Layout />}>
+          <Route index element={<Home />}></Route>
+          <Route path='contact' element={<Contact />}></Route>
+          {/* <Route path='projects' element={<Projects />}></Route> */}
+        </Route>
+      </Routes>
     </>
   )
 }
