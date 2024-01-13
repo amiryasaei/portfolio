355b792ab62b4a0ba679de77d908b7b2b5d5a656
amiryasaei <amiraliyasaei@gmail.com>
2024-01-13 07:36:58 -0800

    fixed some views

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -64,7 +64,7 @@ const Contact = () => {
         </div>
         <div className='footer'>
           You can also download my CV from below!
-          <a href={resume} download><img src={pdfIcon} alt='pdfIcon'/></a>
+          <a href={resume} download><img className="pdfImage"src={pdfIcon} alt='pdfIcon'/></a>
         </div>
       </div>
     </>
