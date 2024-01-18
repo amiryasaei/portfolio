896b3d33666ce1ef00684bec7ca74cdb88662674
amiryasaei <amiraliyasaei@gmail.com>
2024-01-17 19:09:11 -0500

    fixed pdf file

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -71,7 +71,7 @@ const Contact = () => {
         </div>
         <div className='footer'>
           You can also download my CV from below!
-          <a href={resume} download={'AmirYasaeiResume.pdf'} target="_blank" rel="noopener noreferrer"><img className="pdfImage"src={pdfIcon} alt='pdfIcon'/></a>
+          <a href={resume} download={'resume.pdf'} target="_blank" rel="noopener noreferrer"><img className="pdfImage"src={pdfIcon} alt='pdfIcon'/></a>
         </div>
       </div>
     </>
